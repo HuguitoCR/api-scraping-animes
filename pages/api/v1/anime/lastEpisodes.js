2a190b643993b1efb8e4279156ea15a6dd4914dc
HuguitoCR <h.mejias.ugalde53@gmail.com>
2022-01-21 17:09:53 -0600

    v2.4

--- pages/api/v1/anime/lastEpisodes.js
+++ pages/api/v1/anime/lastEpisodes.js
@@ -1,23 +1,22 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-// scraping para obtener los ultimos Episodios agregados https://www.animefenix.com/
-// y guardarlas en un archivo json
 
-export default function handler(req, res) {   
-  axios('https://www.animefenix.com/')
-  .then(response => {
-    const datos = cheerio.load(response.data);
-		const LastEpisodios = [];
 
-		datos('.capitulos-grid .item', response.data).each(function(){
-			const title = datos(this).find('div.overtitle').text().split('\n').join('');
-			const episodios = datos(this).find('div.overepisode').text().split('\n').join('') ;
-      const url = datos(this).find('a').attr('href').split('https://www.animefenix.com/')[1];
-      const imagen = datos(this).find('img').attr('src');
-			LastEpisodios.push({title, episodios, url, imagen});
-		});
+export default function handler(req, res) {
+	axios('https://www.animefenix.com/')
+		.then(response => {
+			const datos = cheerio.load(response.data);
+			const LastEpisodios = [];
 
-    res.status(200).json({ LastEpisodios: LastEpisodios });
-  
-  }).catch(err => console.log(err));
+			datos('.capitulos-grid .item', response.data).each(function() {
+				const title = datos(this).find('div.overtitle').text().split('\n').join('');
+				const episodios = datos(this).find('div.overepisode').text().split('\n').join('') ;
+				const url = datos(this).find('a').attr('href').split('https://www.animefenix.com/')[1];
+				const imagen = datos(this).find('img').attr('src');
+				LastEpisodios.push({ title, episodios, url, imagen });
+			});
+
+			res.status(200).json({ LastEpisodios: LastEpisodios });
+
+		}).catch(err => console.log(err));
 }
--- pages/api/v1/news/reviews.js
+++ pages/api/v1/news/reviews.js
@@ -1,21 +1,20 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-export default function handler(req, res) {  
- 
-    axios('https://somoskudasai.com/')
-     .then(response => {
-       const datos =cheerio.load(response.data);
-           const reviews = [];
+export default function handler(req, res) {
 
-           datos('.ar-reviews .swiper-slide', response.data).each(function(){
-               const title = datos(this).find('a').attr('aria-label');
-               const img = datos(this).find('img').attr('src');
-               const url = datos(this).find('a').attr('href');
-               reviews.push({title, img, url});
-           });
-   
-       res.status(200).json({Reviews: reviews});
-     }).catch(err => console.log(err));
-   }
-   
+	axios('https://somoskudasai.com/')
+		.then(response => {
+			const datos = cheerio.load(response.data);
+			const reviews = [];
+
+			datos('.ar-reviews .swiper-slide', response.data).each(function() {
+				const title = datos(this).find('a').attr('aria-label');
+				const img = datos(this).find('img').attr('src');
+				const url = datos(this).find('a').attr('href');
+				reviews.push({ title, img, url });
+			});
+
+			res.status(200).json({ Reviews: reviews });
+		}).catch(err => console.log(err));
+}
