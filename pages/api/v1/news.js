4ed1d988ed0f1b9c5039ebbb03ac8cc897a3e064
Kevin Arias <kevinjosuearias@hotmail.com>
2022-01-21 16:08:46 -0600

    News ahora contiene las img de los post

--- pages/api/v1/news.js
+++ pages/api/v1/news.js
@@ -10,27 +10,42 @@ export default function handler(req, res) {
     const datos =cheerio.load(response.data);
     const populares = [];
 		const recientes = [];
+		const otros = [];
 		const reviews = [];
 
+		// Populares
     datos('.ar-featured .swiper-slide', response.data).each(function(){
 			const title = datos(this).find('a').attr('aria-label');
+			const img = datos(this).find('img').attr('src');
 			const url = datos(this).find('a').attr('href');
-			populares.push({title, url});
+			populares.push({title, img, url});
 		});
 
-		datos('.news-list .ar', response.data).each(function(){
+		// Recientes
+		datos('.news-list .ar.por', response.data).each(function(){
 			const title = datos(this).find('a').attr('aria-label');
+			const img = datos(this).find('img').attr('src');
 			const url = datos(this).find('a').attr('href');
-			recientes.push({title, url});
+			recientes.push({title, img , url});
 		});
 
+		// Mas noticias
+		datos('div.dg.gt1 article.ar.lg.por', response.data).each(function(){
+			const title = datos(this).find('a').attr('aria-label');
+			const img = datos(this).find('img').attr('src');
+			const url = datos(this).find('a').attr('href');
+			otros.push({title, img, url});
+		});
+
+		// Reviews
 		datos('.ar-reviews .swiper-slide', response.data).each(function(){
 			const title = datos(this).find('a').attr('aria-label');
+			const img = datos(this).find('img').attr('src');
 			const url = datos(this).find('a').attr('href');
-			reviews.push({title, url});
+			reviews.push({title, img, url});
 		});
 
-    res.status(200).json({ recientes: recientes, populares: populares, reviews: reviews });
+    res.status(200).json({ populares: populares, recientes: recientes, reviews: reviews , masNoticias: otros, });
 
   }).catch(err => console.log(err));
 }
