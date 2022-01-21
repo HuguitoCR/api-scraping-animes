6fbade484567b52445b1e9a3f5b3dce63b1a6494
HuguitoCR <h.mejias.ugalde53@gmail.com>
2022-01-21 13:51:59 -0600

    v2 redirects

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   reactStrictMode: true,
+    
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/api',
+        permanent: true,
+      },
+    ]
+  },
+
 }
