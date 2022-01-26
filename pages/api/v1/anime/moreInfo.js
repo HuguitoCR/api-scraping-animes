4386f5868146a49e2e99ce5595d063464cbb8fa4
Kevin Arias <kevinjosuearias@hotmail.com>
2022-01-26 16:07:16 -0600

    moreInfo agregado como nuevo  endpoint

--- pages/api/v1/anime/moreInfo.js
+++ pages/api/v1/anime/moreInfo.js
@@ -10,13 +10,20 @@ export default function handler(req, res) {
 
 			datos('div.columns.is-mobile.is-multiline', response.data).is(function() {
 				const img = datos(this).find('img').attr('src');
-				const estado = datos(this).find('a.button.is-danger').text();
-				const title = datos(this).find('h1.title').text();
+				const estado = datos(this).find('ul.has-text-light').text().split('\n')[2].split(':')[1].replace(' ', '');
+				let siguienteEpisodio;
+				try {
+					siguienteEpisodio = datos(this).find('ul.has-text-light').children('li').children('span.has-text-orange').parent().text().split(':')[1].trim();
+				}
+				catch {
+					siguienteEpisodio = null;
+				}
+				const titulo = datos(this).find('h1.title').text();
 				const sinopsis = datos(this).find('p.sinopsis').text().replace(/\n/g, '').replace(/\"/g, '');
-				const generos = datos(this).find('a.button.is-small').text().replace(' ', '').replace('Á', 'A').split(/(?=[A-Z])/).join(' ').replace('Angeles', 'Ángeles').split(' ');
-				const tipo = datos(this).find('ul.has-text-light').text().trim().split(' ').join('').split('\n')[0].split(':')[1];
-				const totalEpisodios = datos(this).find('ul.has-text-light').text().trim().split(' ').join('').split('\n')[2].split(':')[1];
-				info.push({ img, tipo, totalEpisodios, estado, title, sinopsis, generos });
+				// const generos = datos(this).find('a.button.is-small').text().replace(' ', '').replace('Á', 'A').split(/(?=[A-Z])/).join(' ').replace('Angeles', 'Ángeles').split(' ');
+				const tipo = datos(this).find('ul.has-text-light').text().split('\n')[1].split(':')[1].trimStart();
+				const totalEpisodios = datos(this).find('ul.has-text-light').text().split('\n')[3].split(':')[1].trimStart();
+				info.push({ img, tipo, estado, totalEpisodios, siguienteEpisodio, titulo, sinopsis, generos });
 			});
 
 			datos('ul.anime-page__episode-list li', response.data).each(function() {
