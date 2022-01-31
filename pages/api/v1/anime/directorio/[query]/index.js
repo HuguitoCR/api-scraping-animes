c7929e46c43e454ba9d9fbd503e64f9b5000e8f6
Kevin Arias <kevinjosuearias@hotmail.com>
2022-01-30 22:05:05 -0600

    directorio[query] responde las llamadas correctamente. Tambien se modifico el json para mostrar pagina siguiente y anterior.

--- pages/api/v1/anime/directorio/[query]/index.js
+++ pages/api/v1/anime/directorio/[query]/index.js
@@ -1,48 +1,59 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-
 export default function handler(req, res) {
-	const query = req.query.query;
-	const url = `https://www.animefenix.com/animes?${query}&page=`;
-	const Directorio = [];
-	const inicio = 1;
-	let contador = 0;
+	return new Promise((resolve, reject) => {
+		const url = `https://www.animefenix.com/animes?${req.query.query}`;
+		const Directorio = [];
+		const inicio = 1;
+		let contador = 0;
 
-	console.time('Llamada principal');
-	const getData = async () => {
-	 	const response = await axios(url + inicio);
-		const $ = cheerio.load(response.data);
-		const $paginacion = $('li').last().prev().text();
+		const getData = async () => {
+			const response = await axios(url + inicio);
+			const $ = cheerio.load(response.data);
+			const $paginacion = $('li').last().prev().text();
 
-		const getAnimes = async (page) => {
-			const respu = await axios(url + page);
-			const HTML = cheerio.load(respu.data);
+			const getAnimes = async (page) => {
+				const respu = await axios(url + page);
+				const HTML = cheerio.load(respu.data);
 
-			HTML('.list-series .serie-card', respu.data).each(function() {
-				const id = HTML(this).find('a').attr('href').split('https://www.animefenix.com/')[1];
-				const title = HTML(this).find('a.has-text-orange').text().split('\n').join('');
-				const imagen = HTML(this).find('img').attr('src');
-				const año = HTML(this).find('span.year').text();
-				const estado = HTML(this).find('span.is-orange').text();
-				const tipo = HTML(this).find('span.type').text();
-				const descripcion = HTML(this).find('p').text().split('\n').join('').split('"').join('');
-				Directorio.push({ id, title, imagen, año, estado, tipo, descripcion });
-			});
-			contador++;
+				HTML('.list-series .serie-card', respu.data).each(function() {
+					const id = HTML(this).find('a').attr('href').split('https://www.animefenix.com/')[1];
+					const title = HTML(this).find('a.has-text-orange').text().split('\n').join('');
+					const imagen = HTML(this).find('img').attr('src');
+					const año = HTML(this).find('span.year').text();
+					const estado = HTML(this).find('span.is-orange').text();
+					const tipo = HTML(this).find('span.type').text();
+					const descripcion = HTML(this).find('p').text().split('\n').join('').split('"').join('');
+					Directorio.push({ id, title, imagen, año, estado, tipo, descripcion });
+				});
 
-			if (page < $paginacion && contador < 4) {
-				getAnimes(page + 1);
-			}
-			else {
-				res.status(200).json({ Directorio: Directorio });
-				console.timeEnd('Llamada principal');
-				console.log(Directorio.length);
-				console.log(page);
-				contador = 0;
-			}
+				if (contador < 3) {
+					if (page < $paginacion) {
+						contador++;
+						getAnimes(page + 1);
+					}
+					else {
+						res.status(200).json({ Anterior:`https://api-torii.vercel.app/api/v1/anime/directorio/${req.query.query}&page=${(page - 3)}`,
+							Directorio: Directorio });
+						contador = 0;
+						resolve();
+					}
+				}
+				else {
+					res.status(200)
+						.json({ Siguiente: `https://api-torii.vercel.app/api/v1/anime/directorio/${req.query.query}&page=${(page + 1)}`,
+							Anterior:`https://api-torii.vercel.app/api/v1/anime/directorio/${req.query.query}&page=${(page - 3)}`,
+							Directorio: Directorio });
+					contador = 0;
+					resolve();
+				}
+			};
+			getAnimes(inicio);
 		};
-		getAnimes(inicio);
-	};
-	getData();
+		getData().catch(error => {
+			res.json(error);
+			resolve();
+		});
+	});
 }
