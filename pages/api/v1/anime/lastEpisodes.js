e3ae6803862ad3a9ca5b7c1c9ab095eb0c92d162
Kevin Arias <kevinjosuearias@hotmail.com>
2022-02-01 22:19:55 -0600

    Correccion ESLint

--- pages/api/v1/anime/lastEpisodes.js
+++ pages/api/v1/anime/lastEpisodes.js
@@ -1,22 +1,21 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-
 const Redis = require('ioredis');
 
 export default async function handler(req, res) {
-	 const client = new Redis(process.env.REDIS_URL);
-
+	const client = new Redis(process.env.REDIS_URL);
 	const reply = await client.get('lastEpisodes');
-		 if (reply) {
-			 res.status(200).send(reply);
 
-		 }
+	if (reply) {
+		res.status(200).send({ LastEpisodes: reply });
+	}
 	else {
 		return new Promise((resolve, reject) => {
-		     axios('https://www.animefenix.com/')
+			axios('https://www.animefenix.com/')
 				.then(response => {
 					const datos = cheerio.load(response.data);
 					const LastEpisodios = [];
+
 					datos('.capitulos-grid .item', response.data).each(function() {
 						const title = datos(this).find('div.overtitle').text().split('\n').join('');
 						const episodios = datos(this).find('div.overepisode').text().split('\n').join('') ;
@@ -25,9 +24,9 @@ export default async function handler(req, res) {
 						LastEpisodios.push({ title, episodios, url, imagen });
 					});
 					client.set('lastEpisodes', JSON.stringify(LastEpisodios), 'EX', 1800);
+
 					res.status(200).json({ LastEpisodios: LastEpisodios });
 					resolve();
-
 				})
 				.catch(error => {
 					res.json(error);
