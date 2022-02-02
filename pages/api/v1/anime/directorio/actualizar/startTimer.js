adf782aa87232835f5bcd6767a01f4f422c34d13
Kevin Arias <kevinjosuearias@hotmail.com>
2022-02-02 12:01:21 -0600

    La actualizacion automatica del directorio ahora se encuentra separado, esto se debe a que se detecto que cada vez que se llamaba la consulta del directorio se inciaba un nuevo contador el motivo de la separacion es para cuando se hace un nuevo deploy se pueda activar el timer por separado una vez

---
+++ pages/api/v1/anime/directorio/actualizar/startTimer.js
@@ -0,0 +1,57 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const Redis = require('ioredis');
+
+export default async function handler(req, res) {
+	res.status(200).send({ Mensaje: 'Temporizador iniciado, El directorio se actualizara cada 5 horas' });
+
+	// Actualiza el directorio
+	// se ejecuta cada 5 horas
+	callDirectorio();
+	setInterval(callDirectorio, 18000000);
+}
+
+const callDirectorio = async () => {
+	const client = new Redis(process.env.REDIS_URL);
+	return new Promise((resolve, reject) => {
+		const url = 'https://www.animefenix.com/animes?page=';
+		const Directorio = [];
+		const inicio = 1;
+
+		const getData = async () => {
+			const response = await axios(url + inicio);
+			const $ = cheerio.load(response.data);
+			const $paginacion = $('li').last().prev().text();
+
+			const getAnimes = async (page) => {
+				const respu = await axios(url + page);
+				const HTML = cheerio.load(respu.data);
+
+				HTML('.list-series .serie-card', respu.data).each(function() {
+					const id = HTML(this).find('a').attr('href').split('https://www.animefenix.com/')[1];
+					const title = HTML(this).find('a.has-text-orange').text().split('\n').join('');
+					const imagen = HTML(this).find('img').attr('src');
+					const año = HTML(this).find('span.year').text();
+					const estado = HTML(this).find('span.is-orange').text();
+					const tipo = HTML(this).find('span.type').text();
+					const descripcion = HTML(this).find('p').text().split('\n').join('').split('"').join('');
+					Directorio.push({ id, title, imagen, año, estado, tipo, descripcion });
+				});
+
+				if (page < $paginacion) {
+					getAnimes(page + 1);
+				}
+				else {
+					client.set('directorio', JSON.stringify(Directorio));
+					client.quit();
+					resolve();
+				}
+			};
+			getAnimes(inicio);
+		};
+		getData().catch(error => {
+			res.json(error);
+			resolve();
+		});
+	});
+};
