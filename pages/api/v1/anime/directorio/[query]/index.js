c9fbb01e1f22f73551a3d6b9d238d26ca8f804bc
Kevin Arias <kevinjosuearias@hotmail.com>
2022-02-04 18:14:37 -0600

    Directorio por query modificado, se quito mostrar las paginas. Ya que daba problemas

--- pages/api/v1/anime/directorio/[query]/index.js
+++ pages/api/v1/anime/directorio/[query]/index.js
@@ -3,10 +3,9 @@ const cheerio = require('cheerio');
 
 export default function handler(req, res) {
 	return new Promise((resolve, reject) => {
-		const url = `https://www.animefenix.com/animes?${req.query.query}`;
+		const url = `https://www.animefenix.com/animes?${req.query.query}&page=`;
 		const Directorio = [];
 		const inicio = 1;
-		let contador = 0;
 
 		const getData = async () => {
 			const response = await axios(url + inicio);
@@ -28,24 +27,11 @@ export default function handler(req, res) {
 					Directorio.push({ id, title, imagen, año, estado, tipo, descripcion });
 				});
 
-				if (contador < 3) {
-					if (page < $paginacion) {
-						contador++;
-						getAnimes(page + 1);
-					}
-					else {
-						res.status(200).json({ Anterior:`https://api-torii.vercel.app/api/v1/anime/directorio/${req.query.query}&page=${(page - 3)}`,
-							Directorio: Directorio });
-						contador = 0;
-						resolve();
-					}
+				if (page < $paginacion) {
+					getAnimes(page + 1);
 				}
 				else {
-					res.status(200)
-						.json({ Siguiente: `https://api-torii.vercel.app/api/v1/anime/directorio/${req.query.query}&page=${(page + 1)}`,
-							Anterior:`https://api-torii.vercel.app/api/v1/anime/directorio/${req.query.query}&page=${(page - 3)}`,
-							Directorio: Directorio });
-					contador = 0;
+					res.status(200).json({ Directorio : Directorio });
 					resolve();
 				}
 			};
